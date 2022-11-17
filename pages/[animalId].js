340259085c1d2bf391245e05be9fdd9c60f261eb
DESKTOP-EJPIOLD\dev_byj <myosikb@gmail.com>
2022-11-17 16:59:04 +0900

    change serversigeprops

--- pages/[animalId].js
+++ pages/[animalId].js
@@ -12,39 +12,40 @@ const AnimalID = ({animalId}) => {
 
 export default AnimalID;
 
-// export function getServerSideProps({params: {animalId}}) {
-//     // console.log(ctx);
-//     return {
-//         props: {
-//             animalId
-//         },
-//     }
-// }
-
-export const getStaticProps = async ({params}) => {
-    const animalId = params.animalId;
-    return { 
-      props: {
-        animalId: animalId
-      }
+export function getServerSideProps({params: {animalId}}) {
+    // console.log(ctx);
+    return {
+        props: {
+            animalId
+        },
     }
 }
 
-export const getStaticPaths = async () => {  
-  return {
-    paths: [
-        {params: {animalId: '1'}},
-        {params: {animalId: '2'}},
-        {params: {animalId: '3'}},
-        {params: {animalId: '4'}},
-        {params: {animalId: '5'}},
-        {params: {animalId: '6'}},
-        {params: {animalId: '7'}},
-        {params: {animalId: '8'}},
-        {params: {animalId: '9'}}
-    ],
-    fallback: true
-  }
-}
+// export const getStaticProps = async ({params}) => {
+//   console.log('params..', params);
+//     const animalId = params.animalId;
+//     return { 
+//       props: {
+//         animalId: animalId
+//       }
+//     }
+// }
+
+// export const getStaticPaths = async () => {  
+//   return {
+//     paths: [
+//         {params: {animalId: '1'}},
+//         {params: {animalId: '2'}},
+//         {params: {animalId: '3'}},
+//         {params: {animalId: '4'}},
+//         {params: {animalId: '5'}},
+//         {params: {animalId: '6'}},
+//         {params: {animalId: '7'}},
+//         {params: {animalId: '8'}},
+//         {params: {animalId: '9'}}
+//     ],
+//     fallback: true
+//   }
+// }
 
 // 간혹 Uncaught Error: Cannot find module for page: /[animalId] error 뜸

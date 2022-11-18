bdbfec5525b8cf94cfb4f2822b5d72dc765f8780
DESKTOP-EJPIOLD\dev_byj <myosikb@gmail.com>
2022-11-18 16:37:39 +0900

    Intersection Observer를 활용하여 scroll event 구현

--- Hooks/useScrollFadeIn.js
+++ Hooks/useScrollFadeIn.js
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useCallback, useRef } from 'react';
 
 const useScrollFadeIn = () => {
   const dom = useRef();
@@ -28,6 +28,29 @@ const useScrollFadeIn = () => {
     //   }, [handleScroll]);
 ------------------------------------------------------ */
   
+  const handleScroll = useCallback(([entry]) => {
+    const { current } = dom;
+    if(entry.isIntersecting){
+        // 원하는 이벤트
+    }
+  })
+
+  useEffect(() => {
+    let observer;
+    const { current } = dom;
+    
+    if (current) {
+      // IntersectionObserver에 동작 하게 할 함수와 Observer 세팅 값들을 넘겨줌
+      // threshold는 number나 Array<number>로 정의할 수 있으며, Default 값은 0
+      // number는 TargetElement의 노출 비율을 말하고, 0.7은 70% 정도 노출 되었을 때 해당 이벤트가 실행
+      // Array는 각각의 비율로 노출 될 때마다 함수가 실행
+      observer = new IntersectionObserver(handleScroll, { threshold: 0.7 });
+      observer.observe(current);
+      
+      return () => observer && observer.disconnect();
+    };
+  }, [handleScroll]);
+
   return {
     ref: dom,
     // ref(dom)을 바로 return하지 않고 Object로 한번 감싼 이유는 component에 적용할 때
